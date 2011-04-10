0f303f1037c96fc98d9a97343171bb4acac4d30f
Florian Krüger <florian.krueger@projectserver.org>
2011-04-10 11:55:08 +0200

    added children enum

---
+++ src/dhbw/LWBS/CA5_KB1/model/Children.java
@@ -0,0 +1,34 @@
+package dhbw.LWBS.CA5_KB1.model;
+
+public enum Children
+{
+	NONE(0),	// _
+	ALL(100),	// *
+	ONE(1),
+	TWO(2),
+	THREE(3),
+	FOUR(4),
+	FIVE(5),
+	SIX(6),
+	SEVEN(7),
+	EIGHT(8),
+	NINE(9),
+	TEN(10),
+	ELEVEN(11),
+	TWELVE(12),
+	THIRTEEN(13),
+	FOURTEEN(14),
+	FIFTEEN(15),
+	SIXTEEN(16),
+	SEVENTEEN(17),
+	EIGHTEEN(18),
+	NINETEEN(19),
+	TWENTY(20);
+	
+	private int id;
+	
+	Children(int id)
+	{
+		this.id = id;	
+	}
+}

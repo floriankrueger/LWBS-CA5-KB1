c9faf9962a8da0151cd0eaec50614cf0826500dc
Florian Krüger <florian.krueger@projectserver.org>
2011-04-10 17:47:19 +0200

    first implementation of Star

--- src/dhbw/LWBS/CA5_KB1/model/Star.java
+++ src/dhbw/LWBS/CA5_KB1/model/Star.java
@@ -1,6 +1,32 @@
 package dhbw.LWBS.CA5_KB1.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Star
 {
+	private Set<Concept> specializedConcepts;
+	private Set<Concept> generalizedConcepts;
 
+	Star()
+	{
+		this(new HashSet<Concept>(), new HashSet<Concept>());
+	}
+	
+	Star(Set<Concept> s, Set<Concept> g)
+	{
+		super();
+		this.specializedConcepts = s;
+		this.generalizedConcepts = g;
+	}
+	
+	public boolean addSpecializedConcept(Concept c)
+	{
+		return specializedConcepts.add(c);
+	}
+	
+	public boolean addGeneralizedConcept(Concept c)
+	{
+		return generalizedConcepts.add(c);
+	}
 }

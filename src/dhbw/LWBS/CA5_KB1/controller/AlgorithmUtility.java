14db5bfc7f1ad72acf8617f630d6f7a9c8380abf
Patricia Schwander <pschwander@yahoo.de>
2011-04-10 17:51:12 +0200

    added negExamples function to versionSpaceAlgo

--- src/dhbw/LWBS/CA5_KB1/controller/AlgorithmUtility.java
+++ src/dhbw/LWBS/CA5_KB1/controller/AlgorithmUtility.java
@@ -38,75 +38,125 @@ public class AlgorithmUtility
 			ArrayList<Person> negativeExamples)
 	{
 		// Declarations
-		Set<Person> bV = new LinkedHashSet<Person>(); 	// Menge der bereits vorgelegten Beispiele
-		Set<Concept> s = new LinkedHashSet<Concept>(); 	// Menge der speziellsten Konzepte
-		Set<Concept> g = new LinkedHashSet<Concept>(); 	// Menge der generellsten Konzepte
+		Set<Person> bV = new LinkedHashSet<Person>(); // Menge der bereits vorgelegten Beispiele
+		Set<Concept> s = new LinkedHashSet<Concept>(); // Menge der speziellsten Konzepte
+		Set<Concept> g = new LinkedHashSet<Concept>(); // Menge der generellsten Konzepte
 		// Menge der noch nicht vorgelegten Beispiele entspricht positiveExamples & negative Examples
-		
+
 		// Algorithm
-		
-		s.add(Concept.getMostSpecializedConcept());	// s = {(_,...,_)}
+
+		s.add(Concept.getMostSpecializedConcept()); // s = {(_,...,_)}
 		g.add(Concept.getMostGeneralizedConcept()); // g = {(*,...,*)}
-		
+
 		// Positive Examples
 		for (Person a : positiveExamples) // equivalent zu "solange bR != leere Menge" & "waehle a aus bR"
 		{
-			System.out.println("VS <positive examples>: Person " + a.toConceptString());
-			
+			System.out.println("VS <positive examples>: Person "
+					+ a.toConceptString());
+
 			bV.add(a); // Setze bV = bV vereinigt mit {a}
-			
+
 			for (Concept c : s) // ersetze all H aus G mit H(a) = 0 usw.
 			{
-				System.out.print("VS <positive examples>: Concept " + c + " -> ");
-				generalize(a,c);
+				System.out.print("VS <positive examples>: Concept " + c
+						+ " -> ");
+				generalize(a, c);
 				System.out.println(c);
 			}
 		}
-		
+
 		// Negative Examples
 		for (Person a : negativeExamples) // equivalent zu "solange bR != leere Menge" & "waehle a aus bR"
 		{
 			// Loesche alle Konzepte aus S, die gleich a sind
 			deleteEqualConcepts(s, a);
-			
+
 			// Wenn s leer ist, dann war das letzte negative Beispiel gleich dem letzten Konzept aus s
 			// und der Algorithmus ist fehlgeschlagen
 			if (s.isEmpty())
 			{
 				return null;
 			}
-			
+
 			// Ersetze alle H e G, die a als Beispiel haben, durch die allgemeinste
 			// Spezialisierung von H, die a nicht enhaelt (jedoch alle bisher vorgelegten
 			// positiven Beispiele!)
-			
+
 			Set<Concept> toBeInserted_g = new HashSet<Concept>();
 			Set<Concept> toBeDeleted_g = new HashSet<Concept>();
-			
+
 			for (Concept cG : g)
 			{
 				// Wenn das Beispiel a Teilmenge des aktuellen Konzepts cG ist
 				if (cG.covers(a))
 				{
-					// TODO: Continue
+					// von jedem Element aus s muss ein neues Konzept in g (toBeInserted) erstellt werden
+					// bsp.: s={(a,b)} g={(a,*),(*,b)}
+
+					for (String key : a.getAttributes().keySet())
+					{
+						int a_Attribute = a.getAttributes().get(key);
+						int s_Attribute = s.iterator().next().getAttributes()
+								.get(key);
+
+						if ((a_Attribute != s_Attribute)
+								|| (s_Attribute != 100)) //ALL (*) = 100
+						{
+							Concept c = cG.copy();
+							c.setAttribute(key, s_Attribute);
+							toBeInserted_g.add(c);
+						}
+					}
+					// bisherige Konzepte aus g müssen gelöscht werden
+					toBeDeleted_g.add(cG);
+				}
+
+				for (Concept rG : toBeDeleted_g)
+				{
+					g.remove(rG);
+				}
+
+				//TODO schöner machen :)
+				nextConcept: for (Concept iC : toBeInserted_g)
+				{
+					for (Concept iG : g)
+					{
+						if (iC.covers(iG))
+						{
+							continue nextConcept;
+						}
+					}
+					g.add(iC);
+				}
+				// Wenn g leer ist, dann ist der Algorithmus fehlgeschlagen
+				if (g.isEmpty())
+				{
+					return null;
 				}
 			}
+			if(s.equals(g))
+			{
+				return new Star(s, g);
+			}
 		}
-
 		return null;
 	}
 
 	/**
-	 * Matches every Concept in the Set with the given Person example and removes if they match
+	 * Matches every Concept in the Set with the given Person example and
+	 * removes if they match
 	 * 
-	 * @param s	the Set of Concepts which should be cleared from the given Person example
-	 * @param a the given Person example
+	 * @param s
+	 *            the Set of Concepts which should be cleared from the given
+	 *            Person example
+	 * @param a
+	 *            the given Person example
 	 */
 	private static void deleteEqualConcepts(Set<Concept> s, Person a)
 	{
 		for (Concept cS : s)
 		{
-			if (cS.equals(a)) 
+			if (cS.equals(a))
 			{
 				s.remove(cS);
 			}
@@ -131,7 +181,7 @@ public class AlgorithmUtility
 		// TODO implement
 		return null;
 	}
-	
+
 	// HELPER METHODS
 	private static void generalize(Person p, Concept c)
 	{
@@ -140,37 +190,37 @@ public class AlgorithmUtility
 			c.setAgeClass(p.getAgeClass());
 		else if (p.getAgeClass() != c.getAgeClass())
 			c.setAgeClass(AgeClass.ALL);
-		
+
 		// generalize gender
 		if (c.getGender() == Gender.NONE)
 			c.setGender(p.getGender());
 		else if (p.getGender() != c.getGender())
 			c.setGender(Gender.ALL);
-		
+
 		// generalize married
 		if (c.getMarried() == Married.NONE)
 			c.setMarried(p.getMarried());
 		else if (p.getMarried() != c.getMarried())
 			c.setMarried(Married.ALL);
-		
+
 		// generalize children
 		if (c.getChildren() == Children.NONE)
 			c.setChildren(p.getChildren());
 		else if (p.getChildren() != c.getChildren())
 			c.setChildren(Children.ALL);
-		
+
 		// generalize degree
 		if (c.getDegree() == Degree.NONE)
 			c.setDegree(p.getDegree());
 		else if (p.getDegree() != c.getDegree())
 			c.setDegree(Degree.ALL);
-		
+
 		// generalize profession
 		if (c.getProfession() == Profession.NONE)
 			c.setProfession(p.getProfession());
 		else if (p.getProfession() != c.getProfession())
 			c.setProfession(Profession.ALL);
-		
+
 		// generalize income
 		if (c.getIncome() == Income.NONE)
 			c.setIncome(p.getIncome());

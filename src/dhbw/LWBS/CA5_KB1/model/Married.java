9a03f934f07147b1d5ec664d6b85afb06b626d3a
Florian Krüger <florian.krueger@projectserver.org>
2011-04-10 17:21:32 +0200

    added token and name values and fromString/fromInteger methods (Married)

--- src/dhbw/LWBS/CA5_KB1/model/Married.java
+++ src/dhbw/LWBS/CA5_KB1/model/Married.java
@@ -2,21 +2,55 @@ package dhbw.LWBS.CA5_KB1.model;
 
 public enum Married
 {
-	YES(1),
-	NO(2),
-	NONE(0),
-	ALL(100);
+	YES(1,"ja"),
+	NO(2,"nein"),
+	NONE(0,""),
+	ALL(100,"*");
 
+	public static String NAME = "Married";
 	private int id;
+	private String token;
 
-	private Married(int id)
+	private Married(int id, String token)
 	{
 		this.id = id;
+		this.token = token;
 	}
 
 	public int getId()
 	{
 		return id;
 	}
-
+	
+	public static Married fromInteger(Integer id)
+	{
+		if (id != null)
+		{
+			for (Married a : Married.values())
+			{
+				if (id == a.id)
+				{
+					return a;
+				}
+			}
+		}
+		System.err.println("Unknown Married Id: " + id);
+		return null;
+	}
+	
+	public static Married fromString(String token)
+	{
+		if (token != null)
+		{
+			for (Married a : Married.values())
+			{
+				if (token.equalsIgnoreCase(a.token))
+				{
+					return a;
+				}
+			}
+		}
+		System.err.println("Unknown Married token: " + token);
+		return null;
+	}
 }

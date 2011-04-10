6419649f6b26c10de7e53b387270acde785a7ab9
Florian Krüger <florian.krueger@projectserver.org>
2011-04-10 16:57:51 +0200

    added token and name values and fromString/fromInteger methods should make EnumUtils unnecessary

--- src/dhbw/LWBS/CA5_KB1/model/AgeClass.java
+++ src/dhbw/LWBS/CA5_KB1/model/AgeClass.java
@@ -2,24 +2,59 @@ package dhbw.LWBS.CA5_KB1.model;
 
 public enum AgeClass
 {
-	B18(1), // <18
-	F19T24(2), // 19-24
-	F25T35(3), // 25-35
-	F36T49(4), // 36-49
-	F50T65(5), // 50-65
-	O65(6), // >65
-	NONE(0), // _
-	ALL(100); // *
+	B18(1, "<18"),
+	F19T24(2, "19-24"),
+	F25T35(3, "25-35"),
+	F36T49(4, "36-49"),
+	F50T65(5, "50-65"),
+	O65(6, ">65"),
+	NONE(0, "_"),
+	ALL(100, "*");
 
+	public static String name = "AgeClass";
 	private int id;
+	private String token;
 
-	AgeClass(int id)
+	AgeClass(int id, String token)
 	{
 		this.id = id;
+		this.token = token;
 	}
 
 	public int getId()
 	{
 		return id;
+	}
+
+	public static AgeClass fromInteger(Integer id)
+	{
+		if (id != null)
+		{
+			for (AgeClass a : AgeClass.values())
+			{
+				if (id == a.id)
+				{
+					return a;
+				}
+			}
+		}
+		System.err.println("Unknown AgeClass Id: " + id);
+		return null;
+	}
+	
+	public static AgeClass fromString(String token)
+	{
+		if (token != null)
+		{
+			for (AgeClass a : AgeClass.values())
+			{
+				if (token.equalsIgnoreCase(a.token))
+				{
+					return a;
+				}
+			}
+		}
+		System.err.println("Unknown AgeClass token: " + token);
+		return null;
 	}
 }

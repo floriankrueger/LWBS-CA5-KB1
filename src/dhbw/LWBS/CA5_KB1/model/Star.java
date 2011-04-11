bd8b739782a795a672b87809976eec668f6c6b6d
Patricia Schwander <pschwander@yahoo.de>
2011-04-11 17:33:17 +0200

    addded some comments and some javadoc

--- src/dhbw/LWBS/CA5_KB1/model/Star.java
+++ src/dhbw/LWBS/CA5_KB1/model/Star.java
@@ -10,38 +10,54 @@ public class Star
 	private Set<Concept> specializedConcepts;
 	private Set<Concept> generalizedConcepts;
 
+	/**
+	 * Class constructor
+	 */
 	public Star()
 	{
 		this(new ArrayList<Concept>(), new ArrayList<Concept>());
 	}
-	
+
+	/**
+	 * Class constructor initializing a <code>HashSet</code> with each of the
+	 * given lists
+	 */
 	public Star(List<Concept> s, List<Concept> g)
 	{
 		super();
 		this.specializedConcepts = new HashSet<Concept>(s);
 		this.generalizedConcepts = new HashSet<Concept>(g);
 	}
-	
+
+	//Fill sets with a concept c
 	public boolean addSpecializedConcept(Concept c)
 	{
 		return specializedConcepts.add(c);
 	}
-	
+
 	public boolean addGeneralizedConcept(Concept c)
 	{
 		return generalizedConcepts.add(c);
 	}
-	
+
+	//GETTER for specializedConcepts and generalizedConcepts
 	public Set<Concept> getGeneralizedConcepts()
 	{
 		return generalizedConcepts;
 	}
-	
+
 	public Set<Concept> getSpecializedConcepts()
 	{
 		return specializedConcepts;
 	}
-	
+
+	/**
+	 * Create easily readable output for the <code>Star</code> consisting of
+	 * generalized and specialized <code>Concepts</code>
+	 * 
+	 * @return String which contains both generalized and specialized
+	 *         <code>Concepts</code>
+	 */
 	public String toString()
 	{
 		StringBuffer buffer = new StringBuffer();
